2ffc1940131bdb372968cbeabc0c2422509d77b2
Thaozan123 <rularin@gmail.com>
2026-04-08 20:20:41 +0700

    đã xong sp

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -5,42 +5,111 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ChoThueQuanAo.Controllers
 {
-   public class ProductController : Controller
-   {
-       private readonly AppDbContext _context;
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
 
-       public ProductController(AppDbContext context)
-       {
-           _context = context;
-       }
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
 
-       public async Task<IActionResult> Index()
-       {
-           var products = await _context.Products
-               .Include(p => p.Category)
-               .ToListAsync();
+        public async Task<IActionResult> Index()
+        {
+            var products = await _context.Products
+                .Include(p => p.Category)
+                .ToListAsync();
 
-           return View(products);
-       }
+            return View(products);
+        }
 
-       public IActionResult Create()
-       {
-           ViewBag.Categories = _context.ProductCategories.ToList();
-           return View();
-       }
+        public IActionResult Create()
+        {
+            ViewBag.Categories = _context.ProductCategories.ToList();
+            return View();
+        }
 
-       [HttpPost]
-       public async Task<IActionResult> Create(Product product)
-       {
-           if (ModelState.IsValid)
-           {
-               _context.Products.Add(product);
-               await _context.SaveChangesAsync();
-               return RedirectToAction(nameof(Index));
-           }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Products.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
 
-           ViewBag.Categories = _context.ProductCategories.ToList();
-           return View(product);
-       }
-   }
+            ViewBag.Categories = _context.ProductCategories.ToList();
+            return View(product);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+                return NotFound();
+
+            ViewBag.Categories = _context.ProductCategories.ToList();
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Product product)
+        {
+            if (id != product.Id)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Products.Update(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewBag.Categories = _context.ProductCategories.ToList();
+            return View(product);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var product = await _context.Products
+                .Include(p => p.Category)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (product == null)
+                return NotFound();
+
+            return View(product);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var product = await _context.Products
+                .Include(p => p.Category)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (product == null)
+                return NotFound();
+
+            return View(product);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
 }
